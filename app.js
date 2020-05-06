819481e12c42798e7f81991d77a352967610ac31
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2020-05-06 14:56:26 -0400

    app.js commit for the 3 users in the hardcoded data base

---
+++ app.js
@@ -0,0 +1,30 @@
+//Data
+const data = [
+    {
+        name: 'John Doe', 
+        age: 30, 
+        gender: 'male', 
+        lookingfor: 'female',
+        location: 'Boston, MA',
+        image: 'https://randomuser.me/api/portraits/men/82.jpg'
+    },
+    {
+        name: 'Jenn Smith', 
+        age: 25, 
+        gender: 'female', 
+        lookingfor: 'male',
+        location: 'Miami, FL',
+        image: 'https://randomuser.me/api/portraits/women/82.jpg'
+    },
+    {
+        name: 'William Johnson', 
+        age: 35, 
+        gender: 'male', 
+        lookingfor: 'female',
+        location: 'Lynn, MA',
+        image: 'https://randomuser.me/api/portraits/men/83.jpg'
+    },
+];
+
+
+// Iterator
